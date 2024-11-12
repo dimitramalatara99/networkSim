a5d6c4b30df1fe29ff04793b2088583d97dca11c
Dimitra Malatara <83960924+dimitramalatara99@users.noreply.github.com>
2024-11-12 19:57:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time
-
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial.distance import euclidean
+import simpy as sp
 
 # modeling space for the base staions and the UEs
 array_size = 100
-num_timesteps = 50
-# coordinates for stationary 3 base stations
-bs_positions = np.array([[20, 20], [50, 50], [80, 80]])
+num_timesteps = 100
+# step_size = 0.02
+# coordinates for 3 stationary base stations
+bs_positions = np.array([[50, 80], [25, 30], [75, 30]])
 
 # random coordinates for 10 UEs
 num_ues = 10
@@ -16,22 +16,24 @@ num_ues = 10
 
 # ue_positions = np.random.randint(0, array_size, size=(num_ues, 2))
 def initialize_ues(num_ues, array_size):
-    return np.random.randint(0, array_size, size=(num_ues, 2))
+    return np.random.randint(1, array_size - 1, size=(num_ues, 2))
 
 
-def signal_strength(bs_positions, ue_positions):
-    distance = np.linalg.norm(bs_positions - ue_positions)
-    # distance = cdist(bs_positions, ue_positions, 'euclidean')
-    return 1 / (1 + distance ** 2)
-
+# def signal_strength(bs_positions, ue_positions):
+#     distance = np.linalg.norm(bs_positions - ue_positions)
+#     return 1 / (1 + distance ** 2)
 
 def choose_connection(bs_positions, ue_positions):
     connections = []
     for ue_pos in ue_positions:
         # calculate strength from each base station
-        signals = [signal_strength(bs, ue_pos) for bs in bs_positions]
-        best_bs = np.argmax(signals)  # find the best signal
-        connections.append(best_bs)
+        signals = []
+        for bs in bs_positions:
+            distance = np.linalg.norm(bs - ue_pos)
+            signals.append(1 / (1 + distance ** 2))
+        # signals = [signal_strength(bs, ue_pos) for bs in bs_positions]
+        # best_bs = np.argmax(signals)  # find the best signal
+        connections.append(np.argmax(signals))
     return connections
 
 
@@ -39,21 +41,21 @@ def choose_connection(bs_positions, ue_positions):
 
 def move_ues(ue_positions, array_size):
     for i in range(len(ue_positions)):
-        #ue can move 1 step in 4 directions(up,right,down,left)
-        ue_positions[i] += np.random.randint(-3, 4, size=2)
+        # ue can move 1 step in 4 directions(up,right,down,left)
+        ue_positions[i] += np.random.randint(-10, 10)
         # ues move only inside the aray
-        ue_positions[i] = np.clip(ue_positions[i], 0, array_size - 1)
+        ue_positions[i] = np.clip(ue_positions[i], 1, array_size - 1)
     return ue_positions
 
 
 # SIMULATION
-def simulation(bs_positions, ue_positions, connections, timestep):
-    plt.figure(figsize=(8, 8))
+def simulation(bs_positions, ue_positions, connections, timestep, plt):
     # BS positions in purple
+    plt.rcParams["figure.figsize"] = [8, 8]
     plt.scatter(bs_positions[:, 0], bs_positions[:, 1], c='purple', label='Base Station')
 
     for i, (x, y) in enumerate(bs_positions):
-        #a little to the right
+        # offset to the right
         plt.text(x + 1, y, f"BS-{i}", color='green', fontsize=9)
     # UEs positions in blue
     plt.scatter(ue_positions[:, 0], ue_positions[:, 1], c='blue', label='User Equipment')
@@ -71,20 +73,27 @@ def simulation(bs_positions, ue_positions, connections, timestep):
     plt.ylabel("Y")
     plt.legend()
     plt.title(f"Simulation (Time = {timestep})")
-    plt.show()
+
+    plt.draw()
+    plt.pause(0.3)
+    plt.clf()
+
 
 def simulate_movement(bs_positions, ue_positions, num_timesteps, array_size):
+    plt.figure(figsize=(8, 8))
+
     for timestep in range(num_timesteps):
         print(f"Timestep {timestep + 1}/{num_timesteps}")
 
-        if timestep % 2 == 0:
-            ue_positions = move_ues(ue_positions, array_size)
+        # if timestep % 2 == 0:
+        ue_positions = move_ues(ue_positions, array_size)
 
         connections = choose_connection(bs_positions, ue_positions)
 
         # call simulation function for each new timestep
-        simulation(bs_positions, ue_positions, connections, timestep+1)
-        time.sleep(1)
+        simulation(bs_positions, ue_positions, connections, timestep + 1, plt)
+        # time.sleep(1)
+
 
 # def simulate(bs_positions, ue_positions):
 #     connections = choose_connection(bs_positions, ue_positions)
